3758f3ef85bb589c38799097c4cbba010cc79efd
chanyoung <young9484825@gmail.com>
2020-04-30 21:19:12 +0900

    admin page

--- chan/blog/models.py
+++ chan/blog/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 
 class Post(models.Model):
-    objects = models.Manager()  # class has no objects member오류 때문에 집어넣음
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        # return f"Custom Post object ({self.id})"
+        return "Custom Post object ({})".format(self.id)
