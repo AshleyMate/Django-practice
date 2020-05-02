a846496d501ab5cb7920474bf8d02e8296131b08
chanyoung <young9484825@gmail.com>
2020-05-02 11:38:50 +0900

    media (2)

--- chan/instagram/models.py
+++ chan/instagram/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
-    photo = models.ImageField(blank=True)  # blank가 True라는 것은 꼭 사진을 안넣어도 된다는 것
+    # blank가 True라는 것은 꼭 사진을 안넣어도 된다는 것
+    photo = models.ImageField(blank=True, upload_to='instagram/post/%Y/%m/%d')
     is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
